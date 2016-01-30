24289b6c9c73ab00f12b6718bd51bd855d28d5ec
Vic Lau <xingyulliiuu@gmail.com>
2016-01-30 21:28:39 +0800

    fix the unspecified version problem by using afterEvaluate

--- src/main/groovy/org/swordess/gradle/plugin/SwordessConfigPlugin.groovy
+++ src/main/groovy/org/swordess/gradle/plugin/SwordessConfigPlugin.groovy
@@ -26,64 +26,65 @@ class SwordessConfigPlugin implements Plugin<Project> {
         project.plugins.apply MavenPublishPlugin
         project.plugins.apply BintrayPlugin
 
-        project.repositories {
-            jcenter()
-        }
-        project.dependencies {
-            compile "org.jetbrains.kotlin:kotlin-stdlib:${project.swordess.kotlinVersion}"
-
-            testCompile "junit:junit:${project.swordess.junitVersion}"
-            testCompile "org.jetbrains.kotlin:kotlin-test:${project.swordess.kotlinVersion}"
-        }
-
-        project.sourceSets {
-            main.java.srcDirs += 'src/main/kotlin'
-        }
-
-        project.tasks.create('sourceJar', Jar) {
-            dependsOn project.tasks.getByName('classes')
-            from project.sourceSets.main.allSource
-            classifier 'sources'
-            extension 'jar'
-            group 'build'
-        }
-
-        project.jacoco {
-            toolVersion = project.swordess.jacocoToolVersion
-        }
-
-        project.jacocoTestReport {
-            reports {
-                xml.enabled = true // coveralls plugin depends on xml format report
-                html.enabled = true
+        project.afterEvaluate {
+            project.repositories {
+                jcenter()
             }
-        }
+            project.dependencies {
+                compile "org.jetbrains.kotlin:kotlin-stdlib:${project.swordess.kotlinVersion}"
 
-        project.publishing {
-            publications {
-                swordess(MavenPublication) {
-                    from project.components.java
-                    artifact project.tasks.sourceJar
+                testCompile "junit:junit:${project.swordess.junitVersion}"
+                testCompile "org.jetbrains.kotlin:kotlin-test:${project.swordess.kotlinVersion}"
+            }
+
+            project.sourceSets {
+                main.java.srcDirs += 'src/main/kotlin'
+            }
+
+            project.tasks.create('sourceJar', Jar) {
+                dependsOn project.tasks.getByName('classes')
+                from project.sourceSets.main.allSource
+                classifier 'sources'
+                extension 'jar'
+                group 'build'
+            }
+
+            project.jacoco {
+                toolVersion = project.swordess.jacocoToolVersion
+            }
+
+            project.jacocoTestReport {
+                reports {
+                    xml.enabled = true // coveralls plugin depends on xml format report
+                    html.enabled = true
+                }
+            }
+
+            project.publishing {
+                publications {
+                    swordess(MavenPublication) {
+                        from project.components.java
+                        artifact project.tasks.sourceJar
+                    }
+                }
+            }
+
+            project.bintray {
+                user = project.hasProperty('bintrayUser') ? project.property('bintrayUser') : System.getenv('BINTRAY_USER')
+                key = project.hasProperty('bintrayApiKey') ? project.property('bintrayApiKey') : System.getenv('BINTRAY_API_KEY')
+                publications = ['swordess']
+                pkg {
+                    repo = 'maven'
+                    name = project.name
+                    licenses = ['MIT']
+                    vcsUrl = project.swordess.vcsUrl
+                    version {
+                        name = project.version
+                        released = new Date()
+                    }
                 }
             }
         }
-
-        project.bintray {
-            user = project.hasProperty('bintrayUser') ? project.property('bintrayUser') : System.getenv('BINTRAY_USER')
-            key = project.hasProperty('bintrayApiKey') ? project.property('bintrayApiKey') : System.getenv('BINTRAY_API_KEY')
-            publications = ['swordess']
-            pkg {
-                repo = 'maven'
-                name = project.name
-                licenses = ['MIT']
-                vcsUrl = project.swordess.vcsUrl
-                version {
-                    name = project.version.toString()
-                    released = new Date()
-                }
-            }
-        }
-
     }
 
 }
