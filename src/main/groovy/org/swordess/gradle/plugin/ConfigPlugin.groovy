34609d94641200f31691c9023586566b74efb796
Vic Lau <xingyulliiuu@gmail.com>
2016-01-30 20:19:04 +0800

    0.1.0 version

---
+++ src/main/groovy/org/swordess/gradle/plugin/ConfigPlugin.groovy
@@ -0,0 +1,93 @@
+package org.swordess.gradle.plugin
+
+import com.jfrog.bintray.gradle.BintrayPlugin
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.api.publish.maven.plugins.MavenPublishPlugin
+import org.gradle.api.tasks.bundling.Jar
+import org.gradle.testing.jacoco.plugins.JacocoPlugin
+import org.jetbrains.kotlin.gradle.plugin.KotlinPluginWrapper
+import org.kt3k.gradle.plugin.CoverallsPlugin
+import org.standardout.gradle.plugin.versioneye.VersionEyePlugin
+
+class ConfigPlugin implements Plugin<Project> {
+
+    @Override
+    void apply(Project project) {
+        project.extensions.create("swordess", ConfigPluginExtension)
+
+        project.plugins.apply KotlinPluginWrapper
+
+        // VersionEye
+        project.plugins.apply VersionEyePlugin
+
+        // Unit Test Coverage
+        project.plugins.apply JacocoPlugin
+        project.plugins.apply CoverallsPlugin
+
+        // Packaging and Publishing
+        project.plugins.apply MavenPublishPlugin
+        project.plugins.apply BintrayPlugin
+
+        project.repositories {
+            jcenter()
+        }
+        project.dependencies {
+            compile "org.jetbrains.kotlin:kotlin-stdlib:${project.swordess.kotlinVersion}"
+
+            testCompile "junit:junit:${project.swordess.junitVersion}"
+            testCompile "org.jetbrains.kotlin:kotlin-test:${project.swordess.kotlinVersion}"
+        }
+
+        project.sourceSets {
+            main.java.srcDirs += 'src/main/kotlin'
+        }
+
+        project.tasks.create('sourceJar', Jar) {
+            dependsOn project.tasks.getByName('classes')
+            from project.sourceSets.main.allSource
+            classifier 'sources'
+            extension 'jar'
+            group 'build'
+        }
+
+        project.jacoco {
+            toolVersion = project.swordess.jacocoToolVersion
+        }
+
+        project.jacocoTestReport {
+            reports {
+                xml.enabled = true // coveralls plugin depends on xml format report
+                html.enabled = true
+            }
+        }
+
+        project.publishing {
+            publications {
+                swordess(MavenPublication) {
+                    from project.components.java
+                    artifact project.tasks.sourceJar
+                }
+            }
+        }
+
+        project.bintray {
+            user = project.hasProperty('bintrayUser') ? project.property('bintrayUser') : System.getenv('BINTRAY_USER')
+            key = project.hasProperty('bintrayApiKey') ? project.property('bintrayApiKey') : System.getenv('BINTRAY_API_KEY')
+            publications = ['swordess']
+            pkg {
+                repo = 'maven'
+                name = project.name
+                licenses = ['MIT']
+                vcsUrl = project.swordess.vcsUrl
+                version {
+                    name = project.version
+                    released = new Date()
+                }
+            }
+        }
+
+    }
+
+}
---
+++ src/main/groovy/org/swordess/gradle/plugin/ConfigPluginExtension.groovy
@@ -0,0 +1,11 @@
+package org.swordess.gradle.plugin
+
+class ConfigPluginExtension {
+
+    def String vcsUrl
+
+    def kotlinVersion = '1.0.0-beta-4589'
+    def jacocoToolVersion = '0.7.5.201505241946'
+    def junitVersion = '4.12'
+
+}
